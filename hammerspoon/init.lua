a3173894f032eccb8bf8cf38066ade159aa1db49
ageekymonk <ramzthecoder@gmail.com>
2017-07-31 08:38:41 +1000

    Removing Alt-Tab switch as it is not needed

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -38,33 +38,6 @@ else
     table.insert(buf, "Hammerspoon re-loaded: ")
 end
 
-hs.window.switcher.ui.showSelectedThumbnail = false
-hs.window.switcher.ui.showTitles = false
-switcher = hs.window.switcher.new{'Emacs', 'Google Chrome', 'Rambox'} -- default windowfilter: only visible windows, all Spaces
-
-hs.hotkey.bind('alt','tab',nil,function()switcher:next()end)
--- hs.hotkey.bind('alt','tab','Next window',function()
---                   local choices = {
---                      {
---                         ["text"] = "First Choice",
---                         ["subText"] = "This is the subtext of the first choice",
---                         ["uuid"] = "0001"
---                      },
---                      { ["text"] = "Second Option",
---                         ["subText"] = "I wonder what I should type here?",
---                         ["uuid"] = "Bbbb"
---                      },
---                      { ["text"] = "Third Possibility",
---                         ["subText"] = "What a lot of choosing there is going on here!",
---                         ["uuid"] = "III3"
---                      },
---                   }
---                   x = hs.chooser.new(print)
---                   x:choices(choices)
---                   x:show()
--- end)
--- hs.hotkey.bind('alt-shift','tab','Prev window',function()switcher:previous()end)
-
 table.insert(buf, #modules .. " modules.")
 
 alert.show(table.concat(buf))
